d945ec1bf703083f0f76b105f0f69a24bcd34425
xxxtraptacion <nancyturbolala@gmail.com>
2019-03-16 19:06:27 +0300

    add remove row

--- sobranie/maket/static/js/script.js
+++ sobranie/maket/static/js/script.js
@@ -1,3 +1,5 @@
+
+
 function typeCheck(){
     if(document.getElementById('meetTypeCheck2').checked){
         document.getElementById('partySelect').disabled=false;
@@ -21,28 +23,49 @@ $(function () {
             });
 });
 
-var table = document.querySelector('table.table');
-document.querySelector('button.add-col').onclick = function () {
+
+document.querySelector('button.add-row').onclick = function () {
   var rows = document.getElementById("table").rows;
   for(var i = 0, l = rows.length; i < l; i++) {
     if(i==0){
         var addTD =rows[i].insertCell(-1);
-        addTD.innerHTML = "1";
-        addTD.innerHTML = "0";
+        addTD.innerHTML = $('#timepickerCrMeet').val();
+
+
     }
     else{
         var addTD =rows[i].insertCell(-1);
+        addTD.innerHTML = "0";
     }
   }
 }
 
-document.querySelector('button.add-row').onclick = function () {
+
+$("button").on("click", "button.add-col", function(){
+    var cols = document.getElementById("table").rows[0].cells;
+  var addTR = document.getElementById('table').insertRow(-1);
+  for(var i = 0, l = cols.length; i < l; i++) {
+    if(i==0){
+        var addTD =addTR.insertCell(-1);
+        addTD.innerHTML = $('#datepickerCrMeet').val();
+        addTD.innerHTML+="<button type='button' class='btn btn-danger btn-sm remove-row'>x</button>";
+    }
+    else{
+        var addTD =addTR.insertCell(-1);
+        addTD.innerHTML = "0";
+    }
+  }
+});
+
+
+document.querySelector('button.add-col').onclick = function () {
   var cols = document.getElementById("table").rows[0].cells;
   var addTR = document.getElementById('table').insertRow(-1);
   for(var i = 0, l = cols.length; i < l; i++) {
     if(i==0){
         var addTD =addTR.insertCell(-1);
-        addTD.innerHTML = "1";
+        addTD.innerHTML = $('#datepickerCrMeet').val();
+        addTD.innerHTML+="<button type='button' class='btn btn-danger btn-sm remove-row'>x</button>";
     }
     else{
         var addTD =addTR.insertCell(-1);
@@ -50,3 +73,8 @@ document.querySelector('button.add-row').onclick = function () {
     }
   }
 }
+
+$("table.table").on("click", ".remove-row", function(){
+    var tr= this.parentNode.parentNode;
+    document.getElementById("table").deleteRow(tr.rowIndex);
+});
